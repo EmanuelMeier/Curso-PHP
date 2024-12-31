5c7e97dd1c5ca6755a039708ee77899efc5a87f8
Emanuel Meier Martins <emanuelmmartins@gmail.com>
2024-12-31 11:27:42 -0300

    Variáveis e constantes em PHP

---
+++ cursophp/ex003/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis e constantes</title>
+</head>
+<body>
+    <h1>Exemplo 1</h1>
+    <?php 
+        $nome = "Gustavo";
+        $sobrenome = "Guanabara";
+        const PAIS = "Brasil";
+        //
+        //
+        //
+        //
+       
+        //
+        //
+        //
+        //
+        echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS ;
+    ?>
+</body>
+</html>
---
+++ cursophp/ex004/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Varáveis e constantes</title>
+</head>
+<body>
+    <h1>Exemplo 2</h1>
+     
+    <?php 
+        // $nome = "Pedro";
+        // $Nome = "Maria";
+
+        // echo "É verdade que seu nome é $nome ?"
+
+        // $salário = 2500.75;
+
+        // echo "Seu salário é de R$ $salário";
+
+        // $valorµ = 200;
+
+        // echo "O valor é igual a $valorµ no programa";
+
+        // const _TX = 850;
+
+        // echo "Resultado = " . _TX;
+
+        $nomeCompletoCliente = "Camel Case";
+        $telefone_contato_fornecedor = "Snake Case"
+
+        
+    ?>
+</body>
+</html>
