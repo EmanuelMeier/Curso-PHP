5cac51e43310517510f11525306a9acd0148173d
Emanuel Meier Martins <emanuelmmartins@gmail.com>
2025-01-16 10:08:11 -0300

    Operadores Aritméticos em PHP

---
+++ cursophp/ex008/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritméticos</title>
+</head>
+<body>
+    <h1>Operadores</h1>
+    <?php 
+        // $r = 2 + 2;
+        // $r = "2" . "2";
+        // $r = "Gustavo" + " Guanabara";
+        $r = 5 ** 2;
+        var_dump($r);
+    ?>
+</body>
+</html>
